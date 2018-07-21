c211c1173dd9ab54ab70c8b120d7ca8ef666fa6c
shocklaing <shockwater@gmail.com>
2018-07-21 21:05:02 +0800

    Adding json property for deserialized.

--- Gameplay/Properties/Property.cs
+++ Gameplay/Properties/Property.cs
@@ -1,14 +1,32 @@
 using System;
+using Newtonsoft.Json;
 
 namespace Monopoly.SuperMarioBros.Gameplay.Properties
 {
+    public class PropertiesAsset
+    {
+        [JsonProperty("Properties")]
+        public Property[] Properties { get; set; }
+    }
+    
     public class Property
     {
+        [JsonProperty("Id")]
         public int Id { get; set; }
+
+        [JsonProperty("Colour")]
         public ColourSet Colour { get; set; }
+
+        [JsonProperty("Name")]
         public string Name { get; set; }
+
+        [JsonProperty("Rent")]
         public int Rent { get; set; }
+
+        [JsonProperty("RentWithColourSet")]
         public int RentWithColourSet { get; set; }
+
+        [JsonProperty("Point")]
         public int Point { get; set; }
     }
 }
