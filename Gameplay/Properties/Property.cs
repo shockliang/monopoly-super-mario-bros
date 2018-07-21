f5b8fa638623e12f48a827b86d283db94e9ce7ea
shocklaing <shockwater@gmail.com>
2018-07-21 17:55:12 +0800

    Adding json asset for properties.

--- Gameplay/Properties/Property.cs
+++ Gameplay/Properties/Property.cs
@@ -5,6 +5,7 @@ namespace Monopoly.SuperMarioBros.Gameplay.Properties
     public class Property
     {
         public int Id { get; set; }
+        public ColourSet Colour { get; set; }
         public string Name { get; set; }
         public int Rent { get; set; }
         public int RentWithColourSet { get; set; }
